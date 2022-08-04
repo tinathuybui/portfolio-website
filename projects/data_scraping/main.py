4b737f4c1cdad701aab4090b0de7d1c3488a02ed
Tina Bui <buithuytien_hdvn@yahoo.com>
2022-08-04 22:28:24 +1000

    Update main.py

--- projects/data_scraping/main.py
+++ projects/data_scraping/main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from pandas import DataFrame
 
 URL = "https://realpython.github.io/fake-jobs/"
 resp = requests.get(URL)
@@ -9,11 +10,17 @@ results = soup.find(id="ResultsContainer")
 
 job_elements = results.find_all("div", class_="card-content")
 
+titles = []
+companies = []
+locations = []
+
 for j in job_elements:
     title_element = j.find("h2", class_="title")
     company_element = j.find("h3", class_="company")
     location_element = j.find("p", class_="location")
-    print(title_element.text)
-    print(company_element.text)
-    print(location_element.text.strip())
-    print("\n")
+    titles.append(title_element.text)
+    companies.append(company_element.text)
+    locations.append(location_element.text.strip())
+     
+df = DataFrame({'Title': titles, 'Companies': companies, 'Locations': locations})
+df.to_excel('test.xlsx', sheet_name='sheet1', index=False)
