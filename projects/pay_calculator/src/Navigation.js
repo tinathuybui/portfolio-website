7a0a16b200c8ea8d50d4ab7f4d508002efa5d6de
Tina Bui <buithuytien_hdvn@yahoo.com>
2023-03-06 17:52:55 +1100

    super

---
+++ projects/pay_calculator/src/Navigation.js
@@ -0,0 +1,20 @@
+import { Layout, Menu } from "antd";
+import { Link } from "react-router-dom";
+const { Header } = Layout;
+
+const Navigation = () => {
+	return (
+		<Header>
+			<Menu theme="light" mode="horizontal" defaultSelectedKeys={["1"]}>
+				<Menu.Item key="1">
+					<Link to="/salary">Salary</Link>
+				</Menu.Item>
+				<Menu.Item key="2">
+					<Link to="/super">Super</Link>
+				</Menu.Item>
+			</Menu>
+		</Header>
+	);
+};
+
+export default Navigation;
---
+++ projects/pay_calculator/src/SuperForcast.js
@@ -0,0 +1,23 @@
+// import { InputNumber } from "antd";
+// import { useState } from "react";
+// import { formatter } from "./data/util";
+import "./IncomeForecast.css";
+
+const SuperForcast = () => {
+	// const [anuallPercentage, setAnuallPercentage] = useState();
+	return (
+		<div className="IncomeForecast">
+			<h1>Coming Soon</h1> <br />
+			{/* <InputNumber
+				className="percentage"
+				addonAfter="%"
+				defaultValue={anuallPercentage}
+				onChange={(value) => setAnuallPercentage(value)}
+				formatter={(value) => formatter(value)}
+				placeholder="Salary raise percentage"
+			/> */}
+		</div>
+	);
+};
+
+export default SuperForcast;
